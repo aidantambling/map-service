050a1ae3e6038de162f1b6a9c3e06d367cc264db
Aidan Tambling <atambling@ufl.edu>
2024-03-23 16:13:10 -0400

    Great changes made - no explanation will be provided

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,50 @@ import * as d3 from 'd3';
 import * as topojson from 'topojson-client';
 import './App.scss';
 import Canvas from './components/Canvas';
+import { generateAgeGroupSchema, generateRaceGroupSchema } from './components/schemaGenerators';
 
 const App = () => {
-    // tooltips
+    const [populationURL, setPopulationURL] = useState('https://api.census.gov/data/2020/dec/dp?get=NAME,GEO_ID,DP1_0001C&for=county:*'); // API access URL - passed to Canvas.jsx
+    const [keywordToInfo, setKeywordToInfo] = useState(null); // stores .json data to interpret/access API URLs
+
+    // tooltip refs - passed to Canvas.jsx for value assignment based on mouse-over
     const tooltipCountyRef = useRef(null);
     const tooltipStatRef = useRef(null);
-
+    
+    // used to facilitate scroll over dataset class buttons
     const containerRef = useRef(null);
     const [scrollPosition, setScrollPosition] = useState((0));
 
-    const [buttonData, setButtonData] = useState([]);
-    const buttonsRef = useRef(null);
+    // dataset buttons-related
+    const buttonsRef = useRef(null); // contains dataset and subdataset buttons
+    const [buttonData, setButtonData] = useState([]); // track the dataset buttons that should be rendered
+    const [activeDatasetClassButton, setactiveDatasetClassButton] = useState("All");
+    const [activeDatasetButton, setActiveDatasetButton] = useState("populationDataBtn");
 
+    // dataset context elements
+    const [dataDescription, setDataDescription] = useState("Loading...");
     const [legendData, setLegendData] = useState([]);
-    const [keywordToInfo, setKeywordToInfo] = useState(null)
 
-    const [populationURL, setPopulationURL] = useState('https://api.census.gov/data/2020/dec/dp?get=NAME,GEO_ID,DP1_0001C&for=county:*');
-
+    // changing the view mode
     const [viewingMode, setViewingMode] = useState('quartile');
     const [sliderVal, setSliderVal] =useState(0);
+
+    // initial fetch of API data
     useEffect(() => {
         fetch('/apis.json') // Path relative to the public directory
             .then(response => response.json())
             .then(jsonData => setKeywordToInfo(jsonData.Buttons))
             .catch(error => console.error('Error fetching data:', error));
         }, []);
+
+    // when API is confirmed as fetched, or is changed (?), render the 'All" dataset buttons
+    useEffect(() => {
+        if (keywordToInfo) {
+            renderButtons("All");
+        }
+    }, [keywordToInfo]); 
         
+    // scroll thru the dataset class buttons
     const handleScroll = (scrollAmount) => {
         const newScrollPosition = scrollPosition + scrollAmount;
         if (newScrollPosition > 0 && newScrollPosition < 600){
@@ -36,26 +54,9 @@ const App = () => {
         }
 
         containerRef.current.scrollLeft = newScrollPosition;
-        console.log(newScrollPosition)
     }
 
-    const renderButtons = (buttonID) => {
-        console.log(keywordToInfo[buttonID]);
-
-        const arr = keywordToInfo[buttonID]
-        let newButtonData = [];
-        for (const key in arr){
-            var keyElement = {
-                id : key,
-                url: arr[key].url,
-                label: arr[key].title,
-            }
-            newButtonData.push(keyElement)
-        }
-        console.log(newButtonData)
-        setButtonData(newButtonData);
-    }
-
+    // load only the legend for quartile view
     function renderQuartileData() {
         return (
             <div>
@@ -69,16 +70,88 @@ const App = () => {
         )
     }
 
+    // load the legend (same as quartile view - the legend has alr been updated via useEffect in Canvas.jsx), and load the slider itself as well
     function renderSliderData(){
-        // render only below x, above x, and no dta (3 labels)
-        // render the slider
-        console.log(sliderVal)
         return (
-        <div id='sliderBox'>
-            <input type="range" id="slider" name="slider" min="0" max="100000" step="5000" onChange={(value) => setSliderVal(value)}/>
-            <div id="sliderValue"></div>
-        </div>
+            <>
+                {renderQuartileData()}
+                <div id='sliderBox'>
+                    <input type="range" id="slider" name="slider" min="0" max="100000" step="5000" onChange={(v) => setSliderVal(v.target.value)}/>
+                    <div id="sliderValue">{sliderVal}</div>
+                </div>
+            </>
         );
+    }
+
+    // when a class button is clicked, render its contents (e.g. male, female)
+    const renderButtons = (buttonID) => {
+        let newButtonData = [];
+        const obj = keywordToInfo[buttonID];
+        for (const key in obj) {
+            if (obj.hasOwnProperty(key)) { // Checking if the key is a property of the object
+                const item = obj[key];
+                var keyElement = {
+                    id: key,
+                    url: item.url,
+                    label: item.title,
+                    description: item.description,
+                    children: []
+                };
+                newButtonData.push(keyElement);
+            }
+        }
+        setButtonData(newButtonData); // set the button data (to be rendered) as the data we extracted from the .json
+        setactiveDatasetClassButton(buttonID); // set the class button as active
+    };
+
+    // if a dataset button has children (male, female, hispanic), render its children below it
+    const renderSubButtons = (item) => {
+        setActiveDatasetButton(item.id); // set the dataset button as active
+
+        // for a dataset button w/ no children, just render the map and update the display
+        if (item.id !== "maleDataBtn" && item.id !== "femaleDataBtn" && item.id !== "hispanicBtn" ){
+            setPopulationURL(item.url);
+            setDataDescription(item.description);
+            return;
+        }
+
+        // otherwise, we need to update the button data to display the children buttons
+        let schema;
+        if (item.id === "maleDataBtn" || item.id === "femaleDataBtn") {
+            const gender = item.id === "maleDataBtn" ? "Male" : "Female";
+            const offset = item.id === "maleDataBtn" ? 26 : 50;
+            schema = generateAgeGroupSchema(gender, offset);
+        } else if (item.id === "hispanicBtn"){
+            schema = generateRaceGroupSchema("Hispanic");
+        }
+
+        let newButtonData = Object.keys(schema).map(key => ({
+            id: key,
+            url: schema[key].url,
+            label: schema[key].title,
+            desc: schema[key].description
+        }));
+
+        // Update the children of the category button
+        if (item.children && item.children.length > 0){
+            item.children = [];
+            newButtonData = [];
+            // const updatedButtonData = buttonData.map(button => {
+            //     if (button.id === item.id) {
+            //         return { ...button, children: [] };
+            //     }
+            //     return button;
+            // });
+            // setButtonData(updatedButtonData);
+            // return;
+        }
+        const updatedButtonData = buttonData.map(button => {
+            if (button.id === item.id) {
+                return { ...button, children: newButtonData };
+            }
+            return button;
+        });    
+        setButtonData(updatedButtonData);
     }
 
     return (
@@ -91,7 +164,7 @@ const App = () => {
             </div>
             <div id='bodyContainer'>
                 <div id="canvas-panel">
-                    <Canvas tooltipCountyRef={tooltipCountyRef} tooltipStatRef={tooltipStatRef} setLegendData={setLegendData} populationURL={populationURL}/>
+                    <Canvas tooltipCountyRef={tooltipCountyRef} tooltipStatRef={tooltipStatRef} setLegendData={setLegendData} populationURL={populationURL} sliderVal={sliderVal} viewingMode={viewingMode}/>
                 </div>
                 <div id='info-panel'>
                     <div id="tooltips">
@@ -111,25 +184,32 @@ const App = () => {
 
                         </ul>
                     </div>
-                    <div id='description'>Loading....</div>
+                    <div id='description'>{dataDescription}</div>
                     <div id="selectionContainer">
                         <button id="slideLeft" type="button" onClick={()=>(handleScroll(-150))}>Prev</button>
                         <div id="buttonSelector" ref={containerRef}>
-                            <button className="selectionButton" id="All" onClick={button => renderButtons(button.target.id)}>All Population</button>
-                            <button className="selectionButton" id="Gender" onClick={button => renderButtons(button.target.id)}>Population by Gender</button>
-                            <button className="selectionButton" id="Race" onClick={button => renderButtons(button.target.id)}>Population by Race</button>
-                            <button className="selectionButton" id="Sexuality" onClick={button => renderButtons(button.target.id)}>Population by Sexuality</button>
-                            <button className="selectionButton" id="Institutionalized" onClick={button => renderButtons(button.target.id)}>Institutionalized Population</button>
-                            <button className="selectionButton" id="Median Age" onClick={button => renderButtons(button.target.id)}>Population by Median Age</button>
+                            <button className={activeDatasetClassButton === 'All' ? 'selected-dataset-class-button' : 'dataset-class-button'} id="All" onClick={button => renderButtons(button.target.id)}>All Population</button>
+                            <button className={activeDatasetClassButton === 'Gender' ? 'selected-dataset-class-button' : 'dataset-class-button'} id="Gender" onClick={button => renderButtons(button.target.id)}>Population by Gender</button>
+                            <button className={activeDatasetClassButton === 'Race' ? 'selected-dataset-class-button' : 'dataset-class-button'} id="Race" onClick={button => renderButtons(button.target.id)}>Population by Race</button>
+                            <button className={activeDatasetClassButton === 'Sexuality' ? 'selected-dataset-class-button' : 'dataset-class-button'} id="Sexuality" onClick={button => renderButtons(button.target.id)}>Population by Sexuality</button>
+                            <button className={activeDatasetClassButton === 'Institutionalized' ? 'selected-dataset-class-button' : 'dataset-class-button'} id="Institutionalized" onClick={button => renderButtons(button.target.id)}>Institutionalized Population</button>
+                            <button className={activeDatasetClassButton === 'Median Age' ? 'selected-dataset-class-button' : 'dataset-class-button'} id="Median Age" onClick={button => renderButtons(button.target.id)}>Population by Median Age</button>
                         </div>
                         <button id="slideRight" type="button" onClick={()=>(handleScroll(150))}>Next</button>
                     </div>
                     <div id="boxContainer">
                         <div id="buttonBox" ref={buttonsRef}>
                             {buttonData.map((item, index) => (
-                                <button key={index} className="datasetButton" id={item.id} onClick={() => setPopulationURL(item.url)}>
-                                    {item.label}
-                                </button>
+                                <React.Fragment key={index}>
+                                    <button className={activeDatasetButton === item.id ? 'selectedDatasetButton' : 'datasetButton'} id={item.id} onClick={() => renderSubButtons(item)}>
+                                        {item.label}
+                                    </button>
+                                    {item.children.map((child, childIndex) => (
+                                        <button key={childIndex} className="datasetButton" onClick={() => setPopulationURL(child.url)}>
+                                            {child.label}
+                                        </button>
+                                    ))}
+                                </React.Fragment>
                             ))}
                         </div>
                     </div>
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -3,14 +3,14 @@ import * as d3 from 'd3';
 import * as topojson from 'topojson-client';
 import './canvas.scss';
 
-const Canvas = ({ tooltipCountyRef, tooltipStatRef, setLegendData, populationURL}) => {
+const Canvas = ({ tooltipCountyRef, tooltipStatRef, setLegendData, populationURL, sliderVal, viewingMode}) => {
     const [countyData, setCountyData] = useState([]);
     const [populationData, setPopulationData] = useState([]);
     const [cutoffs, setCutoffs] = useState([]);
 
-    // const populationURL = 'https://api.census.gov/data/2020/dec/dp?get=NAME,GEO_ID,DP1_0001C&for=county:*';
     const countyURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
 
+    // initial map render
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -22,7 +22,6 @@ const Canvas = ({ tooltipCountyRef, tooltipStatRef, setLegendData, populationURL
                 setPopulationData(populationResponse.slice(1));
                 const newCutoffs = getCutoffPoints(populationResponse.slice(1));
                 setCutoffs(newCutoffs);
-                // updateLegend(cutoffs);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -30,10 +29,9 @@ const Canvas = ({ tooltipCountyRef, tooltipStatRef, setLegendData, populationURL
 
         fetchData();
     }, []);
-    
 
+    // re-render the map whenever the API url is changed
     useEffect(() => {
-        // This effect will run whenever `cutoffs` is updated
         console.log('Population Data has been changed to ', populationURL)
         const fetchData = async () => {
             try {
@@ -41,28 +39,19 @@ const Canvas = ({ tooltipCountyRef, tooltipStatRef, setLegendData, populationURL
                 setPopulationData(populationResponse.slice(1));
                 const newCutoffs = getCutoffPoints(populationResponse.slice(1));
                 setCutoffs(newCutoffs);
-                updateLegend(cutoffs);
             } catch(error){
                 console.error('Error fetching data:', error);
             }
         }
-
         fetchData();
-    }, [populationURL]); // Add `cutoffs` as a dependency
+    }, [populationURL]);
 
+    // update the legend whenever we adjust the slider, cutoff points, or viewing mode
     useEffect(() => {
-        // This effect will run whenever `cutoffs` is updated
-        updateLegend(cutoffs);
-    }, [cutoffs]); // Add `cutoffs` as a dependency
+        updateLegend(cutoffs, viewingMode);
+    }, [sliderVal, cutoffs, viewingMode])
 
-    // const updateDataset = async (url) => {
-    //     const populationResponse = await d3.json(url);
-    //     setPopulationData(populationResponse.slice(1));
-    //     const newCutoffs = getCutoffPoints(populationResponse.slice(1));
-    //     setCutoffs(newCutoffs);
-    //     updateLegend(cutoffs);
-    // }
-
+    // whenever we access a new data API, we need to re-compute the cutoff points for quartiles
     function getCutoffPoints(data) {
         const values = data.map(d => +d[2]).sort((a, b) => a - b);
         const quantiles = [];
@@ -75,46 +64,73 @@ const Canvas = ({ tooltipCountyRef, tooltipStatRef, setLegendData, populationURL
       return [0, ...quantiles, values[values.length - 1]];
     }
 
-    function updateLegend(cutoffs) {
-        const colors = ['tomato', 'orange', 'lightgreen', 'limegreen'];
-        const newLegendData = cutoffs.map((cutoff, index) => {
-            if (index < 4){
-                let keyMessage;
-                if (index === 0) {
-                    keyMessage = `Less than ${cutoffs[index + 1]?.toLocaleString()}`;
-                } else if (index === colors.length - 1) {
-                    keyMessage = `More than ${cutoff.toLocaleString()}`;
-                } else {
-                    keyMessage = `${cutoff.toLocaleString()} to ${cutoffs[index + 1]?.toLocaleString()}`;
+    // re-configure the legend
+    function updateLegend(cutoffs, viewingMode) {
+        let newLegendData = [];
+
+        // cutoffs unnecessary - split by slider value
+        if (viewingMode === 'slider'){
+            const colors = ['tomato', 'orange'];
+            newLegendData = [
+                {
+                    color: colors[0],
+                    label: `Below ${sliderVal.toLocaleString()}`
+                },
+                {
+                    color: colors[1],
+                    label: `Above ${sliderVal.toLocaleString()}`
+                },
+                {
+                    color: 'black',
+                    label: 'No data'
                 }
-                return {
-                    color: colors[index],
-                    label: keyMessage, // Add the keyMessage as a property
-                };
-            }
-        }).filter(item => item);
-
-        var blackElement = {
-            'color' : 'black',
-            'label' : 'No data'
+            ];
         }
-        newLegendData.push(blackElement)
-        setLegendData(newLegendData);
+        else {
+            const colors = ['tomato', 'orange', 'lightgreen', 'limegreen'];
+            // Quartile logic
+            newLegendData = cutoffs.map((cutoff, index) => {
+                if (index < colors.length) {
+                    let keyMessage;
+                    if (index === 0) {
+                        keyMessage = `Less than ${cutoffs[index + 1]?.toLocaleString()}`;
+                    } else if (index === colors.length - 1) {
+                        keyMessage = `More than ${cutoff.toLocaleString()}`;
+                    } else {
+                        keyMessage = `${cutoff.toLocaleString()} to ${cutoffs[index + 1]?.toLocaleString()}`;
+                    }
+                    return {
+                        color: colors[index],
+                        label: keyMessage
+                    };
+                }
+            }).filter(item => item);
+            
+            var blackElement = {
+                color: 'black',
+                label: 'No data'
+            };
+            newLegendData.push(blackElement);
+        }
+        setLegendData(newLegendData); // whether the slider or quartile path was used, update the legend
+    }
 
-  }
-
-    const getColor = (percentage) => {
-        if (percentage === 0) return 'black';
+    // given a county, find its color based on the threshold and its value
+    const getColor = (countyVal, sliderVal, viewingMode) => {
+        if (viewingMode === "slider"){
+            return countyVal >= sliderVal ? 'limegreen' : 'tomato';
+        }
+        if (countyVal === 0) return 'black';
         for (let i = 0; i < cutoffs.length - 1; i++) {
-            if (percentage <= cutoffs[i + 1]) {
+            if (countyVal <= cutoffs[i + 1]) {
                 return ['tomato', 'orange', 'lightgreen', 'limegreen'][i];
             }
         }
         return 'limegreen';
     };
 
+    // display county data (name, value) on mouse-over
     const handleMouseOver  = (countyName, stat) => {
-        // console.log(countyName, stat)
         if (tooltipCountyRef.current && tooltipStatRef.current) {
             tooltipCountyRef.current.textContent = countyName;
             tooltipStatRef.current.textContent = stat;
@@ -130,14 +146,14 @@ const Canvas = ({ tooltipCountyRef, tooltipStatRef, setLegendData, populationURL
                     if (fipsCode.startsWith('0')) fipsCode = fipsCode.substring(1);
                     return fipsCode === id;
                 });
-                let percentage = countyItem ? parseFloat(countyItem[2]) : 0;
+                let countyVal = countyItem ? parseFloat(countyItem[2]) : 0;
 
                 return (
                     <path
                         key={index}
                         d={d3.geoPath()(county)}
                         className="county"
-                        fill={getColor(percentage)}
+                        fill={getColor(countyVal, sliderVal, viewingMode)}
                         onMouseOver={(event) => {handleMouseOver(countyItem[0], countyItem[2])
                             }
                         }
