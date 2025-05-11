e7b6cbefc6b581623ab1d9f1fcff7ea5b16f2dac
Aidan Tambling <atambling@ufl.edu>
2025-05-11 19:44:34 -0400

    cleaned up treeview and state references in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import React, { useState, useReducer, useEffect, useRef } from 'react';
 import Canvas from './components/Canvas';
 import { paramToURL } from './components/UrlGenerators';
-import { getVariablesFromConcept, findConceptsFromBase, fetchConceptGroups, findConcepts } from './components/useCensusData';
-import './App.scss';
+import { getVariablesFromConcept, findConceptsFromBase, fetchConceptGroups } from './components/useCensusData';
 import ModalDisplay from './components/Modal/Modal';
 import TreeView from './components/TreeView/TreeView';
 import { Tooltip } from '@mui/material';
 import SettingsModal from './components/SettingsModal/SettingsModal';
 import RangeSlider from './components/RangeSlider/RangeSlider';
 import ViewModal from './components/ViewModal/ViewModal';
-import { styled, alpha } from '@mui/material/styles';
-import { TreeItem, treeItemClasses } from '@mui/x-tree-view/TreeItem';
 import SpinnerLoader from './components/SpinnerLoader/SpinnerLoader';
+import './App.scss';
 
-
-
+// reducer to allow swapping between view mode
 const reducer = (state, action) => {
     switch (action.type) {
         case 'viewQuartile':
@@ -34,6 +31,8 @@ const reducer = (state, action) => {
 
 const App = () => {
     const [state, dispatch] = useReducer(reducer, { viewingMode: 'Quartile', comparisonMode: 'over/under' });
+
+    // manage API access via url(s)
     const [populationURLs, setPopulationURLs] = useState([
         'https://api.census.gov/data/2022/acs/acs5?get=NAME,GEO_ID,B01003_001E&for=county:*'
     ]);
@@ -43,9 +42,11 @@ const App = () => {
     const tooltipStatRef = useRef(null);
 
     // slider state values
-    const [sliderMax, setSliderMax] = useState(100000);
-    const [sliderStep, setSliderStep] = useState(5000);
-    const [sliderVal, setSliderVal] = useState(0);
+    const [sliderSettings, setSliderSettings] = useState({
+        max: 100000,
+        step: 5000,
+        val: 0
+    })
 
     const [dataTitle, setDataTitle] = useState("Loading...");
     const [title, setTitle] = useState('Sex by Age => Total Population')
@@ -57,16 +58,23 @@ const App = () => {
     // changing the view mode
     const [selectedCounty, setSelectedCounty] = useState([]);
 
+    // toggle spinner while canvas loads data
+    const [canvasLoading, setCanvasLoading] = useState(true);
+
+    // color-related state devices
     const [palettes, setPalettes] = useState(null);
     const [selectedPalette, setSelectedPalette] = useState('');
-    const [conceptGroups, setConceptGroups] = useState([]);
-    const [concepts, setConcepts] = useState();
-    const [queryVars, setQueryVars] = useState([]);
-    const [displayedQueryVars, setDisplayedQueryVars] = useState([]); // better way to do this?
 
-    useEffect(() => {
-        setSliderVal("0");
-    }, [populationURLs])
+    // API access
+    const [nameGroups, setNameGroups] = useState([]);
+    const [concepts, setConcepts] = useState();
+
+    const [queryVars, setQueryVars] = useState({
+        current: [],
+        committed: []
+    })
+    // const [queryVars, setQueryVars] = useState([]);
+    // const [displayedQueryVars, setDisplayedQueryVars] = useState([]); // better way to do this?
 
     // load only the legend for quartile view
     function renderQuartileData() {
@@ -84,11 +92,10 @@ const App = () => {
 
     // load the legend (same as quartile view - the legend has alr been updated via useEffect in Canvas.jsx), and load the slider itself as well
     function renderSliderData() {
-
         return (
             <>
                 {renderQuartileData()}
-                <RangeSlider max={sliderMax} step={sliderStep} sliderVal={sliderVal} setSliderVal={setSliderVal} />
+                <RangeSlider max={sliderSettings.max} step={sliderSettings.step} sliderVal={sliderSettings.val} setSliderSettings={setSliderSettings} />
                 <div className='type-selector'>
                     <button onClick={() => dispatch({ type: 'viewOverUnder' })}>Over/Under</button>
                     <button onClick={() => dispatch({ type: 'viewCompRange' })}>Range</button>
@@ -118,35 +125,46 @@ const App = () => {
         );
     }
 
-    const responsive = {
-        superLargeDesktop: {
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-        }
-    };
+    // fetch the stored color palettes and set default
+    const fetchColorPalettes = async () => {
+        const response = await fetch('/palettes.json');
+        const data = await response.json();
+        setPalettes(data);
+        setSelectedPalette(data[0]);
+    }
 
-    // 1. upon loading the dataset, fetch the top-level concept groups to display
     useEffect(() => {
-        fetchConceptGroups().then(conceptGroups => setConceptGroups(conceptGroups));
+        fetchColorPalettes();
     }, [])
 
-    // 2. when a user selects a concept group, display the concepts it refers to
-    // migrated to Modal.jsx
+    // step 1: upon loading the ACS dataset, fetch the 41 top-level nameGroups to display
+    useEffect(() => {
+        fetchConceptGroups().then(nameGroups => setNameGroups(nameGroups));
+    }, [])
+    // these are rendered in Modal.jsx's grid, where user can specify nameGroup and then superConcept
 
-    // 3. when a user selects a concept, display its subconcepts.
-    const renderSubconcepts = async (form) => {
+    // step 2: when a user selects a superConcept, display its concepts.
+    const renderConcepts = async (form) => {
+
+        // function to get the individual labels for the concept(s) under the superconcept
+        const renderLabels = async (formVal, category) => {
+            const pullData = async () => {
+                const fetchedLabels = await getVariablesFromConcept(formVal, category);
+                if (typeof fetchedLabels === 'string') {
+                    try {
+                        return JSON.parse(fetchedLabels);
+                    } catch (e) {
+                        console.error('Failed to parse labels:', e);
+                    }
+                } else {
+                    return fetchedLabels;
+                }
+            }
+            const labels = await pullData();
+            return labels
+        }
+
+        // use the labels to create a tree structure for the search assistant
         const pullData = async () => {
             let c = await findConceptsFromBase(form.slice(0, 6));
 
@@ -161,49 +179,27 @@ const App = () => {
         await pullData();
     }
 
-    // 4. labels are tied to a subconcept. so when the user selects a concept (implicitly selecting subconcept) or explicitly selects subconcept, display the labels
-    const renderLabels = async (formVal, category) => {
-        const pullData = async () => {
-            const fetchedLabels = await getVariablesFromConcept(formVal, category);
-            if (typeof fetchedLabels === 'string') {
-                try {
-                    return JSON.parse(fetchedLabels);
-                } catch (e) {
-                    console.error('Failed to parse labels:', e);
-                }
-            } else {
-                return fetchedLabels;
-            }
-        }
-        const labels = await pullData();
-        return labels
-    }
-
-    const [isToggled, setIsToggled] = useState(true);
-
+    // submit a query to the API using user-selected queryVars
     const queryAPI = () => {
-        const popURLs = queryVars.map(queryVar => paramToURL(queryVar.group));
+        // translate the selected concepts into codes for the API
+        const popURLs = queryVars.current.map(queryVar => paramToURL(queryVar.group));
         setPopulationURLs(popURLs);
-        setDisplayedQueryVars(queryVars);
-        setIsToggled(false);
+
+        // update the legend to reflect the selected concepts
+        setQueryVars(prev => ({
+            ...prev,
+            committed: prev.current
+        }))
+        // setDisplayedQueryVars(queryVars);
+
+        // use loading screen while fetching the new data
+        setCanvasLoading(false);
         setTimeout(() => {
             setTimeout(() => {
-                setIsToggled(true);
+                setCanvasLoading(true);
             }, 1500)
         }, 2000)
-
     }
-
-    const fetchColorPalettes = async () => {
-        const response = await fetch('/palettes.json');
-        const data = await response.json();
-        setPalettes(data);
-        setSelectedPalette(data[0]);
-    }
-
-    useEffect(() => {
-        fetchColorPalettes();
-    }, [])
 
     return (
         <div id="mainContainer">
@@ -213,7 +209,7 @@ const App = () => {
             <div className="settings-container">
                 <div className="modal-container">
                     <Tooltip title={title} PopperProps={{ style: { zIndex: 1 } }}>
-                        <ModalDisplay conceptGroups={conceptGroups} renderSubconcepts={renderSubconcepts} dataTitle={dataTitle} setDataTitle={setDataTitle} setTitle={setTitle} />
+                        <ModalDisplay nameGroups={nameGroups} renderConcepts={renderConcepts} dataTitle={dataTitle} setDataTitle={setDataTitle} setTitle={setTitle} />
                     </Tooltip>
                     <SettingsModal palettes={palettes} setSelectedPalette={setSelectedPalette} selectedPalette={selectedPalette}></SettingsModal>
                     <ViewModal state={state} dispatch={dispatch} />
@@ -222,18 +218,18 @@ const App = () => {
             <div className="canvas-container">
                 <div id="canvas-panel">
                     <>
-                        <div className={isToggled ? 'tempDiv-low' : 'tempDiv-high'}>
-                            {isToggled ?
+                        <div className={canvasLoading ? 'tempDiv-low' : 'tempDiv-high'}>
+                            {canvasLoading ?
                                 <></>
                                 :
                                 <SpinnerLoader showSpinner={true} source={'spinner2.svg'} />}
                         </div>
-                        <Canvas tooltipCountyRef={tooltipCountyRef} tooltipStatRef={tooltipStatRef} setLegendData={setLegendData} populationURLs={populationURLs} sliderVal={sliderVal} state={state} dispatch={dispatch} countOrPercentage={countOrPercentage} setCountOrPercentage={setCountOrPercentage} setSliderMax={setSliderMax} setSliderStep={setSliderStep} selectedCounty={selectedCounty} setSelectedCounty={setSelectedCounty} setDataTitle={setDataTitle} selectedPalette={selectedPalette.colors} />
+                        <Canvas tooltipCountyRef={tooltipCountyRef} tooltipStatRef={tooltipStatRef} setLegendData={setLegendData} populationURLs={populationURLs} sliderVal={sliderSettings.val} state={state} dispatch={dispatch} countOrPercentage={countOrPercentage} setCountOrPercentage={setCountOrPercentage} setSliderSettings={setSliderSettings} selectedCounty={selectedCounty} setSelectedCounty={setSelectedCounty} setDataTitle={setDataTitle} selectedPalette={selectedPalette.colors} />
                         <div id="legend-scale">
                             <h3>{dataTitle}</h3>
                             <div className='currently-viewing-box'>
                                 <div className='query-vars-box'>
-                                    {displayedQueryVars
+                                    {queryVars.committed
                                         .sort((a, b) => {
                                             if (a.group < b.group) return -1;
                                             if (a.group > b.group) return 1;
@@ -269,7 +265,7 @@ const App = () => {
                         {
                             (
                                 <>
-                                    <TreeView items={concepts} style={{ color: 'red' }} setQueryVars={setQueryVars} queryVars={queryVars} />
+                                    <TreeView concepts={concepts} setQueryVars={setQueryVars} />
                                     {
                                         concepts ?
                                             <button onClick={() => {
